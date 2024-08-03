189430f519d64370ddddabc04ea59aee448eddf1
TransloadApp <transloadapp@gmail.com>
2024-08-03 10:04:24 +0700

    Initial Commit

---
+++ FloatingBottomSheet/FloatingBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingBottomSheetApp.swift
+//  FloatingBottomSheet
+//
+//  Created by Nguyen Trong Thach on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
