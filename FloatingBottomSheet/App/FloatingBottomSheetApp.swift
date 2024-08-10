35006c5768065fc03f8a70c32038136b8027588a
Nguyen Trong Thach <stonestudio719@gmail.com>
2024-08-10 17:07:31 +0700

    Updated

---
+++ FloatingBottomSheet/App/FloatingBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingBottomSheetApp.swift
+//  FloatingBottomSheet
+//  https://youtu.be/gxOqwo7bZYE?si=4VB0wXgo8gSR42tI
+//  Created by Nguyen Trong Thach on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
